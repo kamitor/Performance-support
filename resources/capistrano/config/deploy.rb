a6ce1627109091303e3f8de390208390090ecf99
kamitor <christiaan_gerardo@hotmail.com>
2017-09-07 23:33:15 +0200

    Update deploy.rb
    nieuwe capistrano file

--- resources/capistrano/config/deploy.rb
+++ resources/capistrano/config/deploy.rb
@@ -1,46 +1,11 @@
 require 'capistrano/ext/multistage'
-require 'pathname'
+set :stages, ["staging", "production"]
+set :default_stage, "staging"
 
-require 'capistrano/server_definition'
-require 'capistrano/role'
-class Capistrano::Configuration
-  def role_names_for_host(host)
-    roles.map {|role_name, role| role_name if role.include?(host) }.compact || []
-  end
-end
-
-# Stages ('testing' because test is reserved word)
-set :stages, %w(ute uat prod dev)
-
-# liefst wil ik van testing -> ute hebben. Let wel op dat dit goed gaat.
-
-set :default_stage, 'ute'
-set :application_env, 'ute'
-
-# parameters
-#set :parameters_dir, "app/config"
-
-# Symlinks
-set :app_symlinks, ["/www.performancesupport.eu/public_html/media","/dashboard.performancesupport.eu/public_html/media","/dashboard.performancesupport.eu/public_html/pma"]
-
-# application, domain
-set :application, 'ps'
-# Source control tool
+set :application, "ps"
 set :scm, :git
-#set(:branch) { "#{stage_branch}" }
+set :repository, "git@github.com:roanhahn/Performance-support.git"
+set :scm_passphrase, ""
 
-set :repository, 'git@github.com:roanhahn/Performance-support.git'
+set :user, :server-user-name"
 
-set :deploy_via, :remote_cache
-set :keep_releases, 10
-set :run_method, :run
-set :use_sudo, false
-
-# pseudo-tty
-default_run_options[:pty] = true
-
-# Controls how Net::SSH responds to a server key that it does not recognize
-ssh_options[:paranoid] = false
-
-
-after 'ps:release:update'
