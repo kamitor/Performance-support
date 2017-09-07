e76819afda60f68232f75929027be2fff80cccc3
kamitor <christiaan_gerardo@hotmail.com>
2017-09-07 23:53:22 +0200

    Update staging.rb

--- resources/capistrano/config/deploy/staging.rb
+++ resources/capistrano/config/deploy/staging.rb
@@ -1,2 +1,11 @@
-server "172.16.0.11", :app, :web, :db, :primary => true
-set :deploy_to, "/var/www/performance-support_staging"
+set :application_env, 'prod'
+set :deploy_to, '/resources2/fileshare'
+#set :stage_branch,  'prod'
+set :use_sudo, true
+
+role :app, '172.16.0.11', :master => true
+
+# LOGIN
+set :user, 'root'
+set :ssh_options, { :keys => %w( ~/.ssh/id_rsa ), :port => 22, :config => false  }
+set :password, nil # please use ssh public <-> private key auth
