eb6c778bff6e30241d2ccc1c1a107e093bb78427
kamitor <christiaan_gerardo@hotmail.com>
2017-09-07 23:56:51 +0200

    Update deploy.rb

--- resources/capistrano/config/deploy.rb
+++ resources/capistrano/config/deploy.rb
@@ -1,11 +1,20 @@
 require 'capistrano/ext/multistage'
+require 'pathname'
+
+
 set :stages, ["staging", "production"]
 set :default_stage, "staging"
 
 set :application, "ps"
 set :scm, :git
+
+set :deploy_via, :remote_cache
+set :keep_releases, 10
+set :run_method, :run
+set :use_sudo, true
 set :repository, "git@github.com:roanhahn/Performance-support.git"
 set :scm_passphrase, ""
 
 set :user, :server-user-name"
-
+default_run_options[:pty] = true
+ssh_options[:paranoid] = false
